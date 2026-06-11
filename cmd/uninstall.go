9ab625b6eccce9aec86bd759f538398c0c810405
dtbrunoblazeka <bruno.blazeka@dynatrace.com>
2026-06-11 14:03:00 +0200

    Revert "feat(oa): uninstall OneAgent"
    This reverts commit 6912bdf7ff12b4e2eb510ba6552ba717178c37cf.

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -5,9 +5,7 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/dynatrace-oss/dtwiz/pkg/featureflags"
 	"github.com/dynatrace-oss/dtwiz/pkg/installer"
-	"github.com/dynatrace-oss/dtwiz/pkg/installer/oneagent"
 )
 
 var uninstallDryRun bool
@@ -36,13 +34,6 @@ var uninstallOneAgentCmd = &cobra.Command{
 	Short: "Uninstall Dynatrace OneAgent from this host",
 	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if featureflags.IsEnabled(featureflags.OneAgentPoC) {
-			err := oneagent.UninstallOneAgentV2(oneagent.UninstallOptions{DryRun: uninstallDryRun})
-			if errors.Is(err, installer.ErrInstallCancelled) {
-				return nil
-			}
-			return err
-		}
 		return installer.UninstallOneAgent(uninstallDryRun)
 	},
 }
