79e35fa27c81538c512262a4e8e2adcf0d09bd93
NadiiaDziubenko <nadiia.dziubenko@dynatrace.com>
2026-07-16 16:50:46 +0200

    feat: add permissions docs link to setup output

--- cmd/setup.go
+++ cmd/setup.go
@@ -42,6 +42,7 @@ var setupCmd = &cobra.Command{
 		}
 
 		fmt.Printf(" Looking for more deployment options beyond these popular ones? %s\n\n", display.Hyperlink("Visit docs", "https://docs.dynatrace.com/docs/ingest-from"))
+		fmt.Printf(" Learn about needed permissions? %s\n\n", display.Hyperlink("Visit docs", "https://github.com/dynatrace-oss/dtwiz/blob/main/README.md"))
 
 		display.Header("Analyzing system...")
 
